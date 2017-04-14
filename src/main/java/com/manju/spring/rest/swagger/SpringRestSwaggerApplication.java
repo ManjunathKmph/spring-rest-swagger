c9e1a1674867514907c132715ec0ffaed50acb3a
Manjunath Kempaiah <manjunath.kmph@gmail.com>
2017-04-14 14:09:10 +0530

    Updated

--- src/main/java/com/manju/spring/rest/swagger/SpringRestSwaggerApplication.java
+++ src/main/java/com/manju/spring/rest/swagger/SpringRestSwaggerApplication.java
@@ -3,11 +3,16 @@ package com.manju.spring.rest.swagger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Import;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 
 import com.manju.spring.rest.swagger.config.AppConfig;
 
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
 @SpringBootApplication
 @Import(value = AppConfig.class)
+@EnableGlobalMethodSecurity
+@EnableSwagger2
 public class SpringRestSwaggerApplication {
 
 	public static void main(String[] args) {
