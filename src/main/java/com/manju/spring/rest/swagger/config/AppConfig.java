0b6ffb985b5d33e230e1d1c6231003bb55b50a20
Manjunath Kempaiah <manjunath.kmph@gmail.com>
2017-04-14 14:10:36 +0530

    Updated

--- src/main/java/com/manju/spring/rest/swagger/config/AppConfig.java
+++ src/main/java/com/manju/spring/rest/swagger/config/AppConfig.java
@@ -15,7 +15,6 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
 @ComponentScan(basePackages = "com.manju.spring.rest")
-@EnableSwagger2
 public class AppConfig {
 	
 	@Bean
